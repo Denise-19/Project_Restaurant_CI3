646ec69064397fcc3fda09ed5d50ad6b8d2fafc4
Denise Gratia Aruan <denisearuan19@student.uns.ac.id>
2021-07-15 11:22:07 +0700

    first commit

--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="copy-right">
         <div class="container">
-            <p><a target="_blank" >©Kelompok 3</a></p>
+            <p><a target="_blank">©Denise Gratia Aruan</a></p>
             <ul class="social-nav">
                 <li><a href="#"><i class="flaticon-facebook-logo"></i></a></li>
                 <li><a href="#"><i class="flaticon-twitter"></i></a></li>
@@ -14,30 +14,32 @@
 </footer>
 </div><!-- #wrapper -->
 
-	<!-- include jQuery -->
-	
-	<script src="assets/js/jquery.min.js"></script>
-	<!-- bootstrap -->
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	<!-- bootstrap -->
-	<script src="assets/js/owl.carousel.min.js"></script>
-	<!-- slick slider  -->
-	<script src="assets/js/slick.js"></script>
-	<!-- dscountdown  -->
-	<script src="assets/js/dscountdown.min.js"></script>
-	<!-- jquery.nice-select -->
-	<script src="assets/js/jquery.nice-select.js"></script>
-	<!-- magnific-popup -->
-	<script src="assets/js/jquery.magnific-popup.min.js"></script>
-	<!-- Mixitup -->
-	<script src="assets/js/mixitup.min.js"></script>
-	<!-- Google Map -->
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBP1lPhUhDU6MINpruPDinAzXffRlpzzFo"></script>
-	<script src="assets/js/map.js"></script>
-	<!-- custom js -->
-	<script src="assets/js/custom2.js"></script>
-	  <!-- Popper JS -->
-	  <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'></script>
+<!-- include jQuery -->
+
+<script src="assets/js/jquery.min.js"></script>
+<!-- bootstrap -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+</script>
+<!-- bootstrap -->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!-- slick slider  -->
+<script src="assets/js/slick.js"></script>
+<!-- dscountdown  -->
+<script src="assets/js/dscountdown.min.js"></script>
+<!-- jquery.nice-select -->
+<script src="assets/js/jquery.nice-select.js"></script>
+<!-- magnific-popup -->
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<!-- Mixitup -->
+<script src="assets/js/mixitup.min.js"></script>
+<!-- Google Map -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBP1lPhUhDU6MINpruPDinAzXffRlpzzFo"></script>
+<script src="assets/js/map.js"></script>
+<!-- custom js -->
+<script src="assets/js/custom2.js"></script>
+<!-- Popper JS -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'></script>
 
 </body>
 
